c43ca6cb03dd97451a119682664edad859e965ed
MB <blanchet.mathieu30@gmail.com>
2023-04-08 15:53:47 +0200

    redesign

--- view/view.php
+++ view/view.php
@@ -5,6 +5,9 @@
     <title> <?php echo($title); ?>
     </title>
     <link rel="stylesheet" href="../web/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
 </head>
 
 <body>
